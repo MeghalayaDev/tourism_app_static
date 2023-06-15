aef0f2dcb250820a691cbacd3ef01358e3bf1209
Kyrshanlang R Dkhar <bwidu.peace@gmail.com>
2023-06-15 23:25:34 +0530

    Single Card Widget

--- lib/tourist_spots.dart
+++ lib/tourist_spots.dart
@@ -5,27 +5,23 @@ class ListOfTouristSpots extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Card(
-          margin: const EdgeInsets.all(8.0),
-          color: Colors.amber,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(children: [
-              const Icon(Icons.sunny),
-              Text(
-                "Sweet Falls, Meghalaya",
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              const Text(
-                "This waterfall is in the capital city of Meghalaya, it is a beautiful waterfall",
-                textAlign: TextAlign.center,
-              )
-            ]),
+    return Card(
+      margin: const EdgeInsets.all(8.0),
+      color: Colors.amber,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(children: [
+          const Icon(Icons.sunny),
+          Text(
+            "Sweet Falls, Meghalaya",
+            style: Theme.of(context).textTheme.titleLarge,
           ),
-        )
-      ],
+          const Text(
+            "This waterfall is in the capital city of Meghalaya, it is a beautiful waterfall",
+            textAlign: TextAlign.center,
+          )
+        ]),
+      ),
     );
   }
 }
